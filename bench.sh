6159d6d329b71c46d49d6d0f6b3325f772fde419
thanh <thanh@riselabs.xyz>
2025-05-19 16:34:22 +0700

    chore: update bench

--- bench.sh
+++ bench.sh
@@ -2,95 +2,14 @@
 
 txcount=10
 plot_dir="plots"
+name=$1
 
-echo "Benchmarking Arbitrum..."
+echo "Benchmarking $name..."
 echo "Getting the baseline"
-cp arb.env .env && go run . bench resp-time --count $txcount
-echo "Counting the receipt"
-echo "interval=10ms"
-go run . bench receiptcount --txcount $txcount --plot-prefix "arb_count_receipt_10ms" --plot-dir=$plot_dir --plot
-echo "interval=50ms"
-go run . bench receiptcount --txcount $txcount --plot-prefix "arb_count_receipt_50ms" --plot-dir=$plot_dir --plot
-echo "interval=100ms"
-go run . bench receiptcount --txcount $txcount --plot-prefix "arb_count_receipt_100ms" --plot-dir=$plot_dir --plot
+cp $name.env .env && go run . bench resp-time --count $txcount
 
 echo "Total time"
-go run . bench --txcount $txcount --plot-prefix "arb" --plot-dir=$plot_dir --plot
-echo "DONE Arbitrum"
-echo ""
-echo ""
-
-echo "Benchmarking Base..."
-echo "Getting the baseline"
-cp base.env .env && go run . bench resp-time --count $txcount
-echo "Counting the receipt"
-echo "interval=10ms"
-go run . bench receiptcount --txcount $txcount --plot-prefix "base_count_receipt_10ms" --plot-dir=$plot_dir --plot
-echo "interval=50ms"
-go run . bench receiptcount --txcount $txcount --plot-prefix "base_count_receipt_50ms" --plot-dir=$plot_dir --plot
-echo "interval=100ms"
-go run . bench receiptcount --txcount $txcount --plot-prefix "base_count_receipt_100ms" --plot-dir=$plot_dir --plot
-
-echo "Total time"
-go run . bench --txcount $txcount --plot-prefix "base" --plot-dir=$plot_dir --plot
-echo "DONE Base"
-echo ""
-echo ""
-
-echo "Benchmarking MegaETH..."
-echo "Getting the baseline"
-cp mega.env .env && go run . bench resp-time --count $txcount
-echo "Counting the receipt"
-echo "interval=10ms"
-go run . bench receiptcount --txcount $txcount --plot-prefix "mega_count_receipt_10ms" --plot-dir=$plot_dir --plot
-echo "interval=50ms"
-go run . bench receiptcount --txcount $txcount --plot-prefix "mega_count_receipt_50ms" --plot-dir=$plot_dir --plot
-echo "interval=100ms"
-go run . bench receiptcount --txcount $txcount --plot-prefix "mega_count_receipt_100ms" --plot-dir=$plot_dir --plot
-
-echo "Total time async"
-go run . bench --txcount $txcount --plot-prefix "mega_async" --plot-dir=$plot_dir --plot
-
-echo "Total time compare"
-go run . bench compare --txcount $txcount --plot-prefix "mega" --plot-dir=$plot_dir --plot
-echo "DONE MegaETH"
-echo ""
-echo ""
-
-echo "Benchmarking Optimism..."
-echo "Getting the baseline"
-cp op.env .env && go run . bench resp-time --count $txcount
-echo "Counting the receipt"
-echo "interval=10ms"
-go run . bench receiptcount --txcount $txcount --plot-prefix "op_count_receipt_10ms" --plot-dir=$plot_dir --plot
-echo "interval=50ms"
-go run . bench receiptcount --txcount $txcount --plot-prefix "op_count_receipt_50ms" --plot-dir=$plot_dir --plot
-echo "interval=100ms"
-go run . bench receiptcount --txcount $txcount --plot-prefix "op_count_receipt_100ms" --plot-dir=$plot_dir --plot
-
-echo "Total time async"
-go run . bench --txcount $txcount --plot-prefix "op" --plot-dir=$plot_dir --plot
-
-echo "DONE Optimism"
-echo ""
-echo ""
-
-echo "Benchmarking RISE..."
-echo "Getting the baseline"
-cp rise.env .env && go run . bench resp-time --count $txcount
-echo "Counting the receipt"
-echo "interval=10ms"
-go run . bench receiptcount --txcount $txcount --plot-prefix "rise_count_receipt_10ms" --plot-dir=$plot_dir --plot
-echo "interval=50ms"
-go run . bench receiptcount --txcount $txcount --plot-prefix "rise_count_receipt_50ms" --plot-dir=$plot_dir --plot
-echo "interval=100ms"
-go run . bench receiptcount --txcount $txcount --plot-prefix "rise_count_receipt_100ms" --plot-dir=$plot_dir --plot
-
-echo "Total time async"
-go run . bench --txcount $txcount --plot-prefix "rise_async" --plot-dir=$plot_dir --plot
-echo "Total time compare"
-go run . bench compare --txcount $txcount --plot-prefix "rise" --plot-dir=$plot_dir --plot
-
-echo "DONE Rise"
+go run . bench --txcount $txcount --plot-prefix "$name" --plot-dir=$plot_dir --plot
+echo "DONE $name"
 echo ""
 echo ""
--- bench_sync.sh
+++ bench_sync.sh
@@ -6,13 +6,6 @@ plot_dir="plots"
 echo "Benchmarking MegaETH..."
 echo "Getting the baseline"
 cp mega.env .env && go run . bench resp-time --count $txcount
-echo "Counting the receipt"
-echo "interval=10ms"
-go run . bench receiptcount --txcount $txcount --plot-prefix "mega_count_receipt_10ms" --plot-dir=$plot_dir --plot
-echo "interval=50ms"
-go run . bench receiptcount --txcount $txcount --plot-prefix "mega_count_receipt_50ms" --plot-dir=$plot_dir --plot
-echo "interval=100ms"
-go run . bench receiptcount --txcount $txcount --plot-prefix "mega_count_receipt_100ms" --plot-dir=$plot_dir --plot
 
 echo "Total time async"
 go run . bench --txcount $txcount --plot-prefix "mega_async" --plot-dir=$plot_dir --plot
@@ -26,13 +19,6 @@ echo ""
 echo "Benchmarking RISE..."
 echo "Getting the baseline"
 cp rise.env .env && go run . bench resp-time --count $txcount
-echo "Counting the receipt"
-echo "interval=10ms"
-go run . bench receiptcount --txcount $txcount --plot-prefix "rise_count_receipt_10ms" --plot-dir=$plot_dir --plot
-echo "interval=50ms"
-go run . bench receiptcount --txcount $txcount --plot-prefix "rise_count_receipt_50ms" --plot-dir=$plot_dir --plot
-echo "interval=100ms"
-go run . bench receiptcount --txcount $txcount --plot-prefix "rise_count_receipt_100ms" --plot-dir=$plot_dir --plot
 
 echo "Total time async"
 go run . bench --txcount $txcount --plot-prefix "rise_async" --plot-dir=$plot_dir --plot
