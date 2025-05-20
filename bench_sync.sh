527d7d1833106442542e204be66fa279f5a253ec
thanh <thanh@riselabs.xyz>
2025-05-20 11:33:28 +0700

    chore: update bench_sync

--- bench_sync.sh
+++ bench_sync.sh
@@ -10,24 +10,25 @@ plot_dir="plots"
 
 echo "Benchmarking MegaETH..."
 cp mega.env .env
-echo "Total time async"
-go run . bench --txcount $txcount --poll-interval "1ms" --plot-prefix "mega_async" --plot-dir=$plot_dir --plot
-
-echo "Total time sync"
-go run . bench --txcount $txcount --mode "sync" --plot-prefix "mega_sync" --plot-dir=$plot_dir --plot
+#echo "Total time async"
+#go run . bench --txcount $txcount --poll-interval "1ms" --plot-prefix "mega_async" --plot-dir=$plot_dir --plot
+#
+#echo "Total time sync"
+#go run . bench --txcount $txcount --mode "sync" --plot-prefix "mega_sync" --plot-dir=$plot_dir --plot
 
 echo "Total time compare"
-cp rise.env .env
 go run . bench compare --txcount $txcount --poll-interval "1ms" --plot-prefix "mega" --plot-dir=$plot_dir --plot
 echo "DONE MegaETH"
 echo ""
 echo ""
 
 echo "Benchmarking RISE..."
-echo "Total time async"
-go run . bench --txcount $txcount --poll-interval "1ms" --plot-prefix "rise_async" --plot-dir=$plot_dir --plot
-echo "Total time sync"
-go run . bench --txcount $txcount --mode "sync" --plot-prefix "rise_sync" --plot-dir=$plot_dir --plot
+cp rise.env .env
+#echo "Total time async"
+#go run . bench --txcount $txcount --poll-interval "1ms" --plot-prefix "rise_async" --plot-dir=$plot_dir --plot
+#echo "Total time sync"
+#go run . bench --txcount $txcount --mode "sync" --plot-prefix "rise_sync" --plot-dir=$plot_dir --plot
+
 echo "Total time compare"
 go run . bench compare --txcount $txcount --poll-interval "1ms" --plot-prefix "rise" --plot-dir=$plot_dir --plot
 
