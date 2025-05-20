a41c6a20cbc2b2d510de6d682d11f2b9149a4909
thanh <thanh@riselabs.xyz>
2025-05-20 08:55:38 +0700

    chore: update bench_sync

--- bench_sync.sh
+++ bench_sync.sh
@@ -4,6 +4,7 @@ txcount=100
 plot_dir="plots"
 
 echo "Benchmarking MegaETH..."
+cp mega.env .env
 echo "Total time async"
 go run . bench --txcount $txcount --plot-prefix "mega_async" --plot-dir=$plot_dir --plot
 
@@ -11,6 +12,7 @@ echo "Total time sync"
 go run . bench --txcount $txcount --mode "sync" --plot-prefix "mega_sync" --plot-dir=$plot_dir --plot
 
 echo "Total time compare"
+cp rise.env .env
 go run . bench compare --txcount $txcount --plot-prefix "mega" --plot-dir=$plot_dir --plot
 echo "DONE MegaETH"
 echo ""
